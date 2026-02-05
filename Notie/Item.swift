0141ac06575031ab74666cabdf6a33c034f50750
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2026-02-04 23:12:36 -0500

    Initial project setup

---
+++ Notie/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Notie
+//
+//  Created by Neftali Samarey on 2/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NotieTests/NotieTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotieTests.swift
+//  NotieTests
+//
+//  Created by Neftali Samarey on 2/4/26.
+//
+
+import Testing
+@testable import Notie
+
+struct NotieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
