42762aabc6ca99a53da6f41493f8cf26a43fff9b
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2026-02-28 01:10:35 -0500

    Major UI updates

--- Notie/Core/Components/Views/DashboardInfoView.swift
+++ Notie/Core/Components/Views/DashboardInfoView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 public enum IconType: String {
     case calendar
@@ -36,21 +37,36 @@ public struct EntryItem {
     let event: [Event]
 }
 
+public struct OverviewBills: Identifiable {
+    public var id = UUID()
+    let title: String
+    let icon: String
+    let count: String
+}
+
+public enum DashboardViewType {
+    case overview
+    case listItem
+    case grid
+    case none
+}
+
 struct DashboardInfoView: View {
 
-    private let eventName: String
-    private let dueDate: String
+    @Environment(\.colorScheme) private var colorScheme
+    @Query(sort: \EventItem.date, order: .forward) private var events: [EventItem]
 
-    public init(eventName: String, dueDate: String) {
+    private let viewType: DashboardViewType
+    private let eventName: String?
+    private let dueDate: String?
+
+    public init(viewType: DashboardViewType = .none, eventName: String? = nil, dueDate: String? = nil) {
+        self.viewType = viewType
         self.eventName = eventName
         self.dueDate = dueDate
     }
 
-    let test: [Event] = [Event(icon: .calendar, text: "Feb 20, 2026"),
-                         Event(icon: .alert, text: "Due in 10 days")
-                        ]
-
-    let entry: EntryItem = EntryItem(icon: .creditCard, title: "Citi Credit Card", event: [Event(icon: .calendar, text: "March 20, 2026"), Event(icon: .alert, text: "Due in 10 days")])
+    let entry: EntryItem = EntryItem(icon: .creditCard, title: "Con Ed Bill", event: [Event(icon: .calendar, text: "March 8, 2026"), Event(icon: .alert, text: "Due in 7 days")])
 
     var body: some View {
         content
@@ -59,51 +75,202 @@ struct DashboardInfoView: View {
 
 fileprivate extension DashboardInfoView {
 
+    var colorMode: Color {
+        colorScheme == .dark ? Color.darkModeGray : Color.white
+    }
+
     var content: some View {
-        RoundedRectangle(cornerRadius: Constants.cardCornerRadius)
-            .fill(Color.white)
-            .frame(height: Constants.dashboardInfoViewHeight)
-            .frame(maxWidth: .infinity)
-            .overlay(alignment: .leading) {
+        Group {
+            switch viewType {
+            case .overview:
                 VStack(alignment: .leading) {
-                    HStack {
+                    Group {
+                        HStack {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Next Due")
+                                    .foregroundStyle(Color.gray.opacity(0.8))
+                                    .font(.system(size: 12, weight: .medium))
+                                Text(parseEvent(with: events.first).title)
+                                    .font(.system(size: 20, weight: .medium))
+                                Text("Due: \(parseEvent(with: events.first).date.formatted(date: .long, time: .omitted))")
+                                    .font(.system(size: 14, weight: .regular))
+                            }
+
+                            Spacer()
+
+                            VStack(alignment: .center) {
+                                Circle()
+                                    .frame(width: 65, height: 65)
+                                    .foregroundStyle(Color.white)
+                                    .overlay(alignment: .center) {
+                                        VStack {
+                                            Text("1")
+                                                .foregroundStyle(Color.black)
+                                                .font(.system(size: 20, weight: .medium))
+                                            Text("day")
+                                                .foregroundStyle(Color.gray)
+                                                .font(.system(size: 13, weight: .regular))
+                                        }
+                                    }
+                                    .padding(.trailing, 8)
+                                    .padding(.top, 8)
+                                Spacer()
+                            }
+                        }
+
+                        VStack {
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(height: 0.5)
+
+                            Spacer()
+
+                            HStack {
+                                Text("Amount")
+                                    .foregroundStyle(Color.gray.opacity(0.8))
+                                    .font(.system(size: 14, weight: .medium))
+                                Spacer()
+                                Text("$100.00")
+                                    .font(.system(size: 18).bold())
+                            }
+                        }
+                        .padding(.bottom, 8)
+                    }
+                    .foregroundStyle(Color.white)
+                    /*HStack {
                         Image(systemName: entry.icon.iconStringValue)
-                            .font(.system(size: 20, weight: .regular, design: .default))
-                            .foregroundStyle(Color.gradientCleanRed)
-                        Text(entry.title)
-                            .font(.system(size: 20, weight: .semibold, design: .default))
+                            .font(.system(size: 18.5, weight: .regular, design: .default))
+                            .foregroundStyle(Color.gradientPomegranate)
+                        Text(parseEvent(with: events.first).title)
+                            .font(.system(size: 18.5, weight: .semibold, design: .default))
                             .lineLimit(1)
                     }
                     .padding(.vertical, 5)
 
                     Divider()
 
-
                     VStack(alignment: .leading, spacing: 15) {
                         ForEach(entry.event) { item in
                             eventItem(with: item.icon.iconStringValue, and: item.text)
                         }
                     }
-                    .padding(.vertical, 8)
-
-                    Spacer()
+                    .padding(.vertical, 8)*/
                 }
                 .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 169)
+                .background(
+                    RoundedRectangle(cornerRadius: Constants.cardCornerRadius)
+                        .fill(Color.black) // colorMode
+                )
+            case .grid:
+                VStack(alignment: .leading) {
+                    Text("Overview")
+                        .font(.system(size: 18)).fontWeight(.medium)
+
+                    gridItem(numberOfItems: overviewBills)
+                }
+            case .listItem:
+                eventItemList(list: events)
+                    .safeAreaPadding(.bottom, 85)
+            default:
+                EmptyView()
             }
-            //.border(Color.red)
+        }
+    }
+
+    func gridItem(numberOfItems: [OverviewBills] ) -> some View {
+        let columns = [GridItem(.flexible()), GridItem(.flexible())]
+        return LazyVGrid(columns: columns, spacing: 16) {
+            ForEach(numberOfItems) { item in
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.white)
+                    .frame(height: 90)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color(.lightGray).opacity(0.5), lineWidth: 1)
+                    )
+                    .overlay {
+                        VStack(spacing: 10) {
+                            HStack {
+                                Text(item.title)
+                                    .font(.system(size: 13))
+                                    .foregroundStyle(Color.gray)
+                                Spacer()
+                                Image(systemName: item.icon)
+                                    .font(.system(size: 13, weight: .regular, design: .default))
+                                    .foregroundStyle(Color.gray)
+                            }
+                            HStack {
+                                Text(item.count)
+                                    .font(.system(size: 24)).bold()
+                                Spacer()
+                            }
+                        }
+                        .padding(.horizontal, 14)
+                        .padding(.vertical, 14)
+                    }
+            }
+        }
+        .padding(.bottom, 10)
     }
 
     func eventItem(with iconName: String, and description: String) -> some View {
         HStack {
             Image(systemName: iconName)
-                .font(.system(size: 18, weight: .regular, design: .default))
+                .font(.system(size: 16, weight: .regular, design: .default))
             Text(description)
-                .font(.system(size: 18, weight: .regular, design: .default))
+                .font(.system(size: 16, weight: .regular, design: .default))
                 .lineLimit(1)
+        }
+    }
+
+    @ViewBuilder
+    private func eventItemList(list: [EventItem]) -> some View {
+        if list.isEmpty {
+            EmptyView()
+        } else {
+            VStack(alignment: .leading) {
+                Text("Upcoming")
+                    .font(.system(size: 18)).fontWeight(.medium)
+                    .padding(.bottom, 5)
+
+                ForEach(events) { item in
+                    HStack {
+                        Text(item.title)
+                        Spacer()
+                        Text(item.date.formatted(date: .numeric, time: .omitted))
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(
+                        RoundedRectangle(cornerRadius: Constants.cardListItemRadius)
+                            .fill(colorMode)
+                    )
+                }
+            }
         }
     }
 }
 
+fileprivate extension DashboardInfoView {
+
+    func parseEvent(with event: EventItem?) -> (title: String, date: Date) {
+        guard let event = event else { return ("", Date()) }
+        return (event.title, event.date)
+    }
+
+    var overviewBills: [OverviewBills] {
+        let billsCount = 0
+        let billsDueSoon = 0
+        var overview: [OverviewBills] = []
+        overview.append(OverviewBills(title: "Total Bills", icon: "receipt.fill", count: "\(billsCount)"))
+        overview.append(OverviewBills(title: "Due Soon", icon: "clock.badge.fill", count: "\(billsDueSoon)"))
+        return overview
+    }
+}
+
 #Preview {
-    DashboardInfoView(eventName: "AMEX Credit Card", dueDate: "Feb 18, 2026")
+    DashboardInfoView(viewType: .overview, eventName: "AMEX Credit Card", dueDate: "Feb 18, 2026")
+    DashboardInfoView(viewType: .listItem)
 }
--- Notie/Features/Dashboard/DashboardView.swift
+++ Notie/Features/Dashboard/DashboardView.swift
@@ -7,19 +7,49 @@
 
 import SwiftUI
 import SwiftData
+import UserNotifications
+import Observation
 
 struct DashboardView: View {
+
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @Environment(\.colorScheme) private var colorScheme
+
+    @State private var isModalSheetPresented: Bool = false
+    @State private var textValue: String = ""
+    @State private var datePickerDate = Date()
+
+    @Query private var events: [EventItem]
+
+    private let notificationCenter = UNUserNotificationCenter.current()
+    private var backgroundColorScheme: Color { colorScheme == .dark ? Color.gray : Color.mainBackground }
 
     var body: some View {
         mainContent()
             .overlay(alignment: .bottom) {
                 actionButton
             }
+            .task {
+                do {
+                    try await notificationCenter.requestAuthorization(options: [.alert, .badge, .sound])
+                } catch {
+                    print("Request authorization error")
+                }
+            }
+            .sheet(isPresented: $isModalSheetPresented) {
+                modalContent
+                    .presentationDetents([.medium, .large])
+            }
+            .onAppear {
+                print("Event count: \(events.count)")
+                for event in events {
+                    updateDueDateIfPast(for: event, context: modelContext)
+                    print("- Event: \(event.title) on \(event.date.formatted(date: .long, time: .omitted))")
+                }
+            }
     }
 
-    private func addItem() {
+    /*private func addItem() {
         withAnimation {
             let newItem = Item(timestamp: Date())
             modelContext.insert(newItem)
@@ -32,7 +62,7 @@ struct DashboardView: View {
                 modelContext.delete(items[index])
             }
         }
-    }
+    }*/
 }
 
 fileprivate extension DashboardView {
@@ -41,17 +71,22 @@ fileprivate extension DashboardView {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .center, spacing: 10) {
-                    DashboardInfoView(eventName: "Credit Card", dueDate: "Feb 20, 2026")
+                    DashboardInfoView(viewType: .grid)
+                        .frame(maxWidth: .infinity)
+    
+                    DashboardInfoView(viewType: .overview)
                         .frame(maxWidth: .infinity)
 
                     Spacer()
+
+                    DashboardInfoView(viewType: .listItem)
                 }
                 .padding(.horizontal, 15)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .scrollContentBackground(.hidden)
             .background(
-                Color.mainBackground.ignoresSafeArea()
+                backgroundColorScheme.ignoresSafeArea()
             )
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
@@ -59,29 +94,151 @@ fileprivate extension DashboardView {
                         Image(systemName: "gear")
                     }
                 }
-                /*ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: { }) {
-                        Image(systemName: "plus")
-                    }
-                }*/
             }
             .toolbarBackground(.clear, for: .navigationBar)
         }
     }
 
-    var actionButton: some View {
-        Button(action: { /* Add action here if needed */ }) {
+    private var modalContent: some View {
+        VStack(alignment: .center) {
+            Text("Add Item")
+                .font(.system(size: 20, weight: .semibold))
+                .padding(.bottom, 10)
+
+            TextField("Item Name", text: $textValue)
+                .padding(.horizontal, 12)
+                .frame(height: 50)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.secondary.opacity(0.4), lineWidth: 1)
+                )
+
+            // Date picker
+            DatePicker(
+                "Select a Date",
+                selection: $datePickerDate,
+                displayedComponents: [.date]
+            )
+            .datePickerStyle(.compact)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.system(size: 18, weight: .medium))
+            .padding(.vertical, 14)
+
+            Spacer()
+
+            Button {
+                let entry = EventItem(title: textValue, date: datePickerDate)
+                modelContext.insert(entry)
+
+                do {
+                    try modelContext.save()
+                } catch {
+                    print(error)
+                }
+
+                textValue = ""
+                datePickerDate = Date()
+
+                isModalSheetPresented = false
+                /*Task {
+                    //guard !textValue.isEmpty && datePickerDate != Date() else { return }
+                    let entry = EventItem(title: textValue, date: datePickerDate)
+                    /*guard !textValue.isEmpty && datePickerDate != Date() else { return }
+                    let newItem = EventItem(title: textValue, date: datePickerDate)
+                    modelContext.insert(newItem)*/
+                    await saveItem(entry)
+                }*/
+            } label: {
+                Text("Add")
+                    .font(.system(size: 20, weight: .medium))
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.red)
+                    )
+                    .foregroundStyle(.white)
+            }
+            .disabled(textValue.isEmpty ? true : false)
+            .opacity(textValue.isEmpty ? 0.5 : 1.0)
+        }
+        .padding(.vertical, 20)
+        .padding(.horizontal, 20)
+    }
+
+    private var actionButton: some View {
+        Button(action: { isModalSheetPresented.toggle() }) {
             Image(systemName: "plus")
                 .foregroundColor(.white)
                 .font(.system(size: 24, weight: .bold))
                 .frame(width: 56, height: 56)
-                .background(Circle().fill(Color.gradientCleanRed))
+                .background(Circle().fill(Color.black))
+                //.background(Circle().fill(Color.gradientPomegranate))
                 .shadow(radius: 2)
         }
     }
 }
 
+fileprivate extension DashboardView {
+
+    @MainActor
+    func saveItem(_ item: EventItem) async {
+        modelContext.insert(item)
+    }
+
+    func updateDueDateIfPast(for item: EventItem, context: ModelContext) {
+        let calendar = Calendar.current
+        let now = Date()
+
+        guard item.date < now else { return }
+
+        let components = calendar.dateComponents([.month], from: item.date, to: now)
+        let monthsToAdd = (components.month ?? 0) + 1
+
+        if let newDate = calendar.date(byAdding: .month, value: monthsToAdd, to: item.date) {
+            item.date = newDate
+            try? context.save()
+        }
+    }
+    /*func updateDueDateIfPast(for item: EventItem, context: ModelContext) {
+        let calendar = Calendar.current
+        let now = Date()
+
+        guard item.date < now else { return }
+
+        var updatedDate = item.date
+
+        while updatedDate < now {
+            if let nextMonth = calendar.date(byAdding: .month, value: 1, to: updatedDate) {
+                updatedDate = nextMonth
+            } else {
+                break
+            }
+        }
+
+        item.date = updatedDate
+
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save updated date:", error)
+        }
+    }*/
+}
+
 #Preview {
     DashboardView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: EventItem.self, inMemory: true)
 }
+
+//@Observable
+//final class DashboardViewModel {
+//
+//    let database: EventStore
+//
+//    init() {
+//        // If EventStore() can throw, keep try! for now to match existing intent. Consider proper error handling later.
+//        self.database = try! EventStore()
+//    }
+//}
+
