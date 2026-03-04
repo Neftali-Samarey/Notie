eacde9b6778c4f9c045c455c4b0813688529ec11
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2026-03-04 00:33:56 -0500

    Added settings view

--- Notie/Features/Settings/SettingsView.swift
+++ Notie/Features/Settings/SettingsView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Text("Notifications")
+                Text("Days from Due Date")
+            }
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
